cadea7dac0527bf56c75a21d3eb522845c0bc5e0
Lior Shefer <lior.shefer@wix.com>
2016-09-14 15:04:14 +0300

    also viewer code

--- build/viewer.js
+++ build/viewer.js
@@ -10,25 +10,47 @@ function createControllers (controllerConfigs) {
     });
 }
 
-function foo () {
-    debugger;
-}
-
 var controller = function () {
     return {
         start: function start($w) {
+            debugger;
+
+
             const bot = $w('@wix-bot');
             if (bot.length > 0) {
                 bot.show();
             }
-            const gallery = $w('@pro-gallery');
+            const gallery = $w('@gallery');
             if (gallery.length > 0) {
-                gallery.onReady().then(api => {
-                    api.getUrl().then(url => {
-                        debugger;
+                images = gallery.images;
+
+
+                fetch('http://localhost:8000/images/', {
+                    method: 'post',
+                    headers: new Headers({
+                        'Content-Type': 'application/json'
+                    }),
+                    body: JSON.stringify({ images: images })
+                })
+                    .then(response => {
+                        return response.text();
+                    })
+                    .then(text => {
+
                     });
-                });
+
+
+
+
+                //wix.navigate.to('/page2');
             }
+
+            /*
+            const img = $w('@image');
+            if (img.length > 0) {
+                img.url = images[2].url
+            }
+            */
         }
     }
 };
