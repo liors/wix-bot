cc07962658d56ac4758cb655abea2e964161e794
Lior Shefer <lior.shefer@wix.com>
2016-09-14 15:00:00 +0300

    adding data and script

---
+++ server/data.js
@@ -0,0 +1,89 @@
+'use strict';
+const _ = require('lodash');
+
+
+let imagesSources;
+let Data = {
+    set: (images) => {
+        imagesSources = images;
+
+    },
+
+    getImageFor: (keyword) => {
+        // dump non-words
+        keyword = keyword.replace(/[^\w]/g, ' ');
+        // dump multiple white-space
+        keyword = keyword.replace(/\s+/g, ' ');
+        // split
+        var wordlist = keyword.split(' ');
+        console.log('before   => ' + wordlist);
+        console.log('size   => ' + wordlist.length);
+        var stopwords = [ "a", "about", "above", "accordingly", "after",
+            "again", "against", "ah", "all", "also", "although", "always", "am", "among", "amongst", "an",
+            "and", "any", "anymore", "anyone", "are", "as", "at", "away", "be", "been",
+            "begin", "beginning", "beginnings", "begins", "begone", "begun", "being",
+            "below", "between", "but", "by", "ca", "can", "cannot", "come", "could",
+            "did", "do", "doing", "during", "each", "either", "else", "end", "et",
+            "etc", "even", "ever", "far", "ff", "following", "for", "from", "further", "furthermore",
+            "get", "go", "goes", "going", "got", "had", "has", "have", "he", "her",
+            "hers", "herself", "him", "himself", "his", "how", "i", "if", "in", "into",
+            "is", "it", "its", "itself", "last", "lastly", "less", "many", "may", "me",
+            "might", "more", "must", "my", "myself", "near", "nearly", "never", "new",
+            "next", "no", "not", "now", "o", "of", "off", "often", "oh", "on", "only",
+            "or", "other", "otherwise", "our", "ourselves", "out", "over", "perhaps",
+            "put", "puts", "quite", "s", "said", "saw", "say", "see", "seen", "shall",
+            "she", "should", "since", "so", "some", "such", "t", "than", "that", "the",
+            "their", "them", "themselves", "then", "there", "therefore", "these", "they",
+            "this", "those", "though", "throughout", "thus", "to", "too",
+            "toward", "unless", "until", "up", "upon", "us", "ve", "very", "was", "we",
+            "were", "what", "whatever", "when", "where", "which", "while", "who",
+            "whom", "whomever", "whose", "why", "with", "within", "without", "would",
+            "yes", "your", "yours", "yourself", "yourselves", ",", ";", "?", ".", "$", " ", "example", "using"];
+
+        console.log('stopwords size   => ' + stopwords.length);
+        //iterate over the total words and remove the stopwords
+        for(var i=0; i<wordlist.length; i++) {
+            console.log('checking if ' + wordlist[i] + ' is a stopword');
+            if (isStopword(stopwords, wordlist[i])) {
+                wordlist.splice(i, 1);
+            }
+        }
+        console.log('images =>' + imagesSources.length);
+        console.log('after   => ' + wordlist);
+
+
+        var results = [];
+        _.forEach(wordlist, (word)=> {
+            results.push(getImg(imagesSources, word))
+        });
+
+        return results;
+    }
+};
+
+function getImg (imagesSources, keyword) {
+    return _.find(imagesSources, (img) => {
+        if (_.includes(img.title.toUpperCase(), keyword)) {
+            console.log(`title: ${img.title.toUpperCase()}, keyword ${keyword}`);
+            return img;
+        }
+    })
+}
+
+
+function isStopword(stopWordArray, element) {
+    var flag = 0;
+    for (var i = 0; i < stopWordArray.length; i++) {
+        if (stopWordArray[i].toLowerCase() === element.toLowerCase()) {
+            return true;
+        }
+        if (element == " " || element == "") {
+            return true;
+        }
+    }
+    if (flag == 0) {
+       return false;
+    }
+}
+
+module.exports = Data;
--- server/script.js
+++ server/script.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const Script = require('smooch-bot').Script;
 const scriptRules = require('./script.json');
 const imageRecognition = require('./imageRecognition');
+const data = require('./data');
 
 module.exports = new Script({
     processing: {
@@ -48,9 +49,16 @@ module.exports = new Script({
 
                 if (!_.has(scriptRules, upperText)) {
                     if (upperText.startsWith("I'M LOOKING FOR")) {
-                        return imageRecognition.process(upperText.split("I'M LOOKING FOR")[1].split(' ')).then((data) => {
-                            bot.say('I found a pic here\'s a <a href="http://google.com">link</a>').then(()=> 'speak');
-                        });
+                        const normalizedkeywords = upperText.split("I'M LOOKING FOR")[1].trim();
+                        const link = data.getImageFor(normalizedkeywords);//'Designer Living Room');
+                        console.log('got a link.... ' + link);
+
+
+
+                        return bot.say('I found something check it out! <a href=' + link.src + '>link.src</a>').then(()=> 'speak');
+                        //return imageRecognition.process(upperText.split("I'M LOOKING FOR")[1].split(' ')).then((data) => {
+
+                        //});
 
                     } else {
                         return bot.say(`I didn't understand that.`).then(() => 'speak');
